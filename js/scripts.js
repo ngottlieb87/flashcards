a86a2829af446761dbc9161ef34c7377517385f4
Margaret Berry and Noah Gottlieb <noah@>
2017-08-08 15:20:10 -0700

    add classes for individual buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,82 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".displayTerm").toggle();
-    $(".hideTerm").toggle();
+  $(".clickable1").click(function() {
+    $(".display1").toggle();
+    $(".hide1").toggle();
   });
   $(".clickable2").click(function() {
     $(".display2").toggle();
     $(".hide2").toggle();
+  });
+  $(".clickable3").click(function() {
+    $(".display3").toggle();
+    $(".hide3").toggle();
+  });
+  $(".clickable4").click(function() {
+    $(".display4").toggle();
+    $(".hide4").toggle();
+  });
+  $(".clickable5").click(function() {
+    $(".display5").toggle();
+    $(".hide5").toggle();
+  });
+  $(".clickable6").click(function() {
+    $(".display6").toggle();
+    $(".hide6").toggle();
+  });
+  $(".clickable7").click(function() {
+    $(".display7").toggle();
+    $(".hide7").toggle();
+  });
+  $(".clickable8").click(function() {
+    $(".display8").toggle();
+    $(".hide8").toggle();
+  });
+  $(".clickable9").click(function() {
+    $(".display9").toggle();
+    $(".hide9").toggle();
+  });
+  $(".clickable10").click(function() {
+    $(".display10").toggle();
+    $(".hide10").toggle();
+  });
+  $(".clickable11").click(function() {
+    $(".display11").toggle();
+    $(".hide11").toggle();
+  });
+  $(".clickable12").click(function() {
+    $(".display12").toggle();
+    $(".hide12").toggle();
+  });
+  $(".clickable13").click(function() {
+    $(".display13").toggle();
+    $(".hide13").toggle();
+  });
+  $(".clickable14").click(function() {
+    $(".display14").toggle();
+    $(".hide14").toggle();
+  });
+  $(".clickable15").click(function() {
+    $(".display15").toggle();
+    $(".hide15").toggle();
+  });
+  $(".clickable16").click(function() {
+    $(".display16").toggle();
+    $(".hide16").toggle();
+  });
+  $(".clickable17").click(function() {
+    $(".display17").toggle();
+    $(".hide17").toggle();
+  });
+  $(".clickable18").click(function() {
+    $(".display18").toggle();
+    $(".hide18").toggle();
+  });
+  $(".clickable19").click(function() {
+    $(".display19").toggle();
+    $(".hide19").toggle();
+  });
+  $(".clickable20").click(function() {
+    $(".display20").toggle();
+    $(".hide20").toggle();
   });
 });
