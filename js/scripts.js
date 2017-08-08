c2e46d35dd9b4127ca209cd1175566a9dd53493d
Margaret Berry and Noah Gottlieb <margaret@>
2017-08-08 14:06:38 -0700

    hide and display definition on click for different cards independtly.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".displayTerm").toggle();
+    $(".hideTerm").toggle();
+  });
+  $(".clickable2").click(function() {
+    $(".display2").toggle();
+    $(".hide2").toggle();
+  });
+});
