c700bac07ace06c74f0c2cfdfcea6eb90df05a57
Margaret Berry and Noah Gottlieb <noah@>
2017-08-08 16:44:09 -0700

    add buttons and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,82 +1,88 @@
 $(document).ready(function() {
   $(".clickable1").click(function() {
-    $(".display1").toggle();
-    $(".hide1").toggle();
+    $(".display1").slideToggle(600);
+    $(".hide1").slideToggle(600);
   });
   $(".clickable2").click(function() {
-    $(".display2").toggle();
-    $(".hide2").toggle();
+    $(".display2").slideToggle(600);
+    $(".hide2").slideToggle(600);
   });
   $(".clickable3").click(function() {
-    $(".display3").toggle();
-    $(".hide3").toggle();
+    $(".display3").slideToggle(600);
+    $(".hide3").slideToggle(600);
   });
   $(".clickable4").click(function() {
-    $(".display4").toggle();
-    $(".hide4").toggle();
+    $(".display4").slideToggle(600);
+    $(".hide4").slideToggle(600);
   });
   $(".clickable5").click(function() {
-    $(".display5").toggle();
-    $(".hide5").toggle();
+    $(".display5").slideToggle(600);
+    $(".hide5").slideToggle(600);
   });
   $(".clickable6").click(function() {
-    $(".display6").toggle();
-    $(".hide6").toggle();
+    $(".display6").slideToggle(600);
+    $(".hide6").slideToggle(600);
   });
   $(".clickable7").click(function() {
-    $(".display7").toggle();
-    $(".hide7").toggle();
+    $(".display7").slideToggle(600);
+    $(".hide7").slideToggle(600);
   });
   $(".clickable8").click(function() {
-    $(".display8").toggle();
-    $(".hide8").toggle();
+    $(".display8").slideToggle(600);
+    $(".hide8").slideToggle(600);
   });
   $(".clickable9").click(function() {
-    $(".display9").toggle();
-    $(".hide9").toggle();
+    $(".display9").slideToggle(600);
+    $(".hide9").slideToggle(600);
   });
   $(".clickable10").click(function() {
-    $(".display10").toggle();
-    $(".hide10").toggle();
+    $(".display10").slideToggle(600);
+    $(".hide10").slideToggle(600);
   });
   $(".clickable11").click(function() {
-    $(".display11").toggle();
-    $(".hide11").toggle();
+    $(".display11").slideToggle(600);
+    $(".hide11").slideToggle(600);
   });
   $(".clickable12").click(function() {
-    $(".display12").toggle();
-    $(".hide12").toggle();
+    $(".display12").slideToggle(600);
+    $(".hide12").slideToggle(600);
   });
   $(".clickable13").click(function() {
-    $(".display13").toggle();
-    $(".hide13").toggle();
+    $(".display13").slideToggle(600);
+    $(".hide13").slideToggle(600);
   });
   $(".clickable14").click(function() {
-    $(".display14").toggle();
-    $(".hide14").toggle();
+    $(".display14").slideToggle(600);
+    $(".hide14").slideToggle(600);
   });
   $(".clickable15").click(function() {
-    $(".display15").toggle();
-    $(".hide15").toggle();
+    $(".display15").slideToggle(600);
+    $(".hide15").slideToggle(600);
   });
   $(".clickable16").click(function() {
-    $(".display16").toggle();
-    $(".hide16").toggle();
+    $(".display16").slideToggle(600);
+    $(".hide16").slideToggle(600);
   });
   $(".clickable17").click(function() {
-    $(".display17").toggle();
-    $(".hide17").toggle();
+    $(".display17").slideToggle(600);
+    $(".hide17").slideToggle(600);
   });
   $(".clickable18").click(function() {
-    $(".display18").toggle();
-    $(".hide18").toggle();
+    $(".display18").slideToggle(600);
+    $(".hide18").slideToggle(600);
   });
   $(".clickable19").click(function() {
-    $(".display19").toggle();
-    $(".hide19").toggle();
+    $(".display19").slideToggle(600);
+    $(".hide19").slideToggle(600);
   });
   $(".clickable20").click(function() {
-    $(".display20").toggle();
-    $(".hide20").toggle();
+    $(".display20").slideToggle(600);
+    $(".hide20").slideToggle(600);
+  });
+  $("button").click(function(){
+    $("ul").append("<li>You're doing great!</li>");
+  });
+  $("button#Clear").click(function(){
+    $("li").remove();
   });
 });
